7f32a7cd35ad1f2233d531bd09be29128e3e508c
Haruka Kotani <harukakotani28@gmail.com>
2023-03-29 22:55:45 -0700

    added owner respo

---
+++ PokemonReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,29 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private DataContext _context;
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Owner GetOwner(int ownerId)
+        {
+            return _context.Owners.Where(o => o.Id == ownerId).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public bool OwnersExist(int ownerId)
+        {
+            return _context.Owners.Any(o => o.Id == ownerId);
+        }
+    }
+}
